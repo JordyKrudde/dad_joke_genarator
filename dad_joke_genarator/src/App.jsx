be4ee595c716542e2c48ea299d917f7f6f88b2bd
Jordy krude <jordy@Jordys-MacBook-Pro.local>
2022-12-14 11:47:44 +0100

    installed vite & tailwind

---
+++ dad_joke_genarator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import JokeGenarator from './JokeGenarator'
+import axios from 'axios'
+
+function App() {
+
+  return (
+    <JokeGenarator />
+  )
+}
+
+export default App
---
+++ dad_joke_genarator/src/JokeGenarator.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function JokeGenarator() {
+    return(
+    <>  
+    <div class="container">
+        <h3>Don't laugh challenge</h3>
+        <div id="joke" class="joke">
+            // joke goes here
+        </div>
+        <button id="genarate_joke" class="">
+            Next Joke
+        </button>
+    </div>
+    </>
+    )
+}
