e4a035c3a6dd629f3961c3d2878f9c4f68571cf4
Jordy krude <jordy@Jordys-MacBook-Pro.local>
2022-12-14 11:53:43 +0100

    delete template css

--- dad_joke_genarator/src/JokeGenarator.jsx
+++ dad_joke_genarator/src/JokeGenarator.jsx
@@ -8,7 +8,7 @@ export default function JokeGenarator() {
         <div id="joke" class="joke">
             // joke goes here
         </div>
-        <button id="genarate_joke" class="">
+        <button id="genarate_joke" class="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
             Next Joke
         </button>
     </div>
