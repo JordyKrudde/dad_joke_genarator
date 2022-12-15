580dacf2bdca5205838d89565b77383a2c0daffa
Jordy krude <jordy@Jordys-MacBook-Pro.local>
2022-12-15 11:20:02 +0100

    add api call & button for next joke

--- dad_joke_genarator/src/App.jsx
+++ dad_joke_genarator/src/App.jsx
@@ -3,11 +3,38 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import JokeGenarator from './JokeGenarator'
 import axios from 'axios'
+import { useEffect } from 'react'
+
+async function getJoke() {
+  const request =  await  axios.get('https://icanhazdadjoke.com/', {
+        headers: {
+          'Accept': 'application/json'
+    }
+  })
+  return request.data.joke 
+}
 
 function App() {
+  const [joke, setJoke] = useState(null)
+  
+  useEffect(() => {
+    async function fetchJoke() {
+      const joke = await getJoke();
+      setJoke(joke);
+    }
+    fetchJoke();
+  }, [])
+
+  function nextJoke() {
+    async function fetchJoke() {
+      const joke = await getJoke();
+      setJoke(joke);
+    }
+    fetchJoke();
+  }
 
   return (
-    <JokeGenarator />
+    <JokeGenarator joke={joke} nextJoke={nextJoke}/>
   )
 }
 
--- dad_joke_genarator/src/JokeGenarator.jsx
+++ dad_joke_genarator/src/JokeGenarator.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function JokeGenarator() {
+export default function JokeGenarator({ joke, nextJoke }) {
     return(
     <>  
     <div className='bg-slate-500 grid h-screen place-items-center'>
         <div className='text-center bg-white py-12 px-24 rounded-xl'>
             <h3 className='text-3xl'>Dad joke JokeGenarator</h3>
-            <div id="joke" class="text-3xl my-4">
-                // joke goes here
+            <div id="joke" className='text-3xl my-4'>
+                <p>{joke}</p>
             </div>
-            <button id="genarate_joke" class="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 mt-4 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
+            <button onClick={nextJoke} className='inline-block rounded-lg bg-indigo-600 px-4 py-1.5 mt-4 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700'>
                 Next Joke
             </button>
         </div>
